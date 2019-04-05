834ace232defa54c15c7b6db202553fc307fa8a0
Karl-Heinz Krachenfels <karl-heinz@krachenfels.de>
2019-04-05 22:50:13 +0200

    timer activation callback

--- sketch_ntp_loop.ino
+++ sketch_ntp_loop.ino
@@ -5,11 +5,11 @@
 #include <ArduinoJson.h>
 #include <LinkedList.h>
 
-
-
 ESP8266WebServer server(80);
-int ledPin = 16;  //D0
-bool ledState = HIGH;
+int ledPin0 = D0;   // D0
+int ledPin1 = D1;  // D1
+bool ledState0 = LOW;
+bool ledState1 = LOW;
 const int sensorPin = A0; //14
 const int humidityPin = 15;  // find include file for A1 for NodeMCU board#
 
@@ -41,12 +41,58 @@ class Timer {
     
     Timer(uint start_hour, uint start_minute, uint end_hour, uint end_minute, uint relais_num) : 
           start_hour(start_hour), start_minute(start_minute), 
-          end_hour(start_hour), end_minute(start_minute), 
+          end_hour(start_hour), end_minute(end_minute), 
           relais_num(relais_num), state(STATE_INACTIVE)
-    {
-    };   
+    {};   
+
+    boolean isBeforeStartTime(uint hour, uint minute){
+      if( hour< start_hour)
+        return true;
+      else if( hour==start_hour && minute < start_minute)
+        return true;
+      return false;
+    }
+
+    boolean isAfterEndTime(uint hour, uint minute){
+      if( hour> end_hour)
+        return true;
+      else if( hour==end_hour && minute > end_minute)
+        return true;
+      return false;
+    }
 };
 LinkedList<Timer*> *timerList;
+
+class Condition{
+  private:
+  public:
+    virtual bool check(uint temp, uint humidity) =0;
+    virtual String description()=0;
+};
+
+class Condition0{
+    private:
+  public:
+    virtual bool check(uint temp, uint humidity){
+      return true;
+    }
+    virtual String description(){
+      return "true";
+    }
+};
+
+class Condition1{
+    private:
+  public:
+    virtual bool check(uint temp, uint humidity){
+      return temp>5 && humidity<10;
+    }
+    virtual String description(){
+      return "temp>5 && humidity<10";
+    }
+};
+
+LinkedList<Condition*> *conditionList;
 
 // ------- SENSORS -------------
 
@@ -63,10 +109,35 @@ float getHumidity(){
 }
 
 // ------- REST CALLS -------------
-void toggle(){
-  ledState = !ledState;
-  digitalWrite(ledPin, ledState);
-  server.send(200, "text/plain", "LED toggled");
+void toggle_0(){
+  ledState0 = !ledState0;
+  digitalWrite(ledPin0, ledState0);
+  server.send(200, "text/plain", "LED0 toggled");
+}
+void toggle_1(){
+  ledState1 = !ledState1;
+  digitalWrite(ledPin1, ledState1);
+  server.send(200, "text/plain", "LED1 toggled");
+}
+void on_0(){
+  ledState0 = HIGH;
+  digitalWrite(ledPin0, ledState0);
+  server.send(200, "text/plain", "LED0 HIGH");
+}
+void on_1(){
+  ledState1 = HIGH;
+  digitalWrite(ledPin1, ledState1);
+  server.send(200, "text/plain", "LED1 HIGH");
+}
+void off_0(){
+  ledState0 = LOW;
+  digitalWrite(ledPin0, ledState0);
+  server.send(200, "text/plain", "LED0 LOW");
+}
+void off_1(){
+  ledState1 = LOW;
+  digitalWrite(ledPin1, ledState1);
+  server.send(200, "text/plain", "LED1 LOW");
 }
 
 void readSensors(){
@@ -81,7 +152,7 @@ void readSensors(){
 }
 
 LinkedList<Timer*>* initTimers(){
-    Timer *t1 = new Timer(10, 0, 10, 10, 0);
+    Timer *t1 = new Timer(16, 30, 16, 59, 0);
     Timer *t2 = new Timer(11, 0, 11, 10, 1);
     LinkedList<Timer*>* timerList =  new LinkedList<Timer*>();
     timerList->add(t1);
@@ -89,7 +160,6 @@ LinkedList<Timer*>* initTimers(){
     return timerList;
 }
 void timers(){
-
     DynamicJsonDocument doc(1024);
     JsonObject root = doc.to<JsonObject>();
     JsonArray timers = root.createNestedArray("timers");
@@ -162,9 +232,9 @@ uint32_t updateTimes(){
   uint32_t actualTime = timeUNIX + (currentMillis - lastNTPResponse)/1000;
   if (actualTime != prevActualTime && timeUNIX != 0) { 
     prevActualTime = actualTime;
-    Serial.printf("\rUTC time:\t%d:%d:%d   ", getHours(actualTime), 
-                  getMinutes(actualTime), getSeconds(actualTime));
-    Serial.println();
+    //Serial.printf("\rUTC time:\t%d:%d:%d   ", getHours(actualTime), 
+    //              getMinutes(actualTime), getSeconds(actualTime));
+    //Serial.println();
   }  
   return actualTime;
 }
@@ -202,17 +272,34 @@ void triggerCallbacks( uint32_t actualTime ){
       return;
     int actualHour = getHours(actualTime);
     int actualMinute =getMinutes(actualTime);
-        Serial.print(actualHour);
+    Serial.print(actualHour);
     Serial.print(":");
     Serial.println(actualMinute);
 
+    
   for(int i=0; i<timerList->size(); i++){
     Timer *timer=timerList->get(i);
+
+    Serial.print("Timer ");
     Serial.print(i);
     Serial.print(". ");
     Serial.print(timer->start_hour);
     Serial.print(":");
-    Serial.println(timer->start_minute);
+    Serial.print(timer->start_minute);
+    Serial.print(" - ");
+    Serial.print(timer->end_hour);
+    Serial.print(":");
+    Serial.print(timer->end_minute);
+    Serial.print(" ");
+
+    if(timer->isBeforeStartTime(actualHour, actualMinute) ){
+      Serial.println(" not yet reached");
+    } else if(timer->isAfterEndTime(actualHour, actualMinute) ){
+      Serial.println(" already passed");
+    } else {
+      Serial.println(" active");
+    }
+    
   }
 }
 
@@ -228,25 +315,22 @@ void setup() {
   startWiFi();                   
   startUDP();
 
-  //for(int i=0; i<10; i++)
-  //  timers();
-
   if(!WiFi.hostByName(NTPServerName, timeServerIP)) { 
     Serial.println("DNS lookup failed. Rebooting.");
     Serial.flush();
     ESP.reset();
   }
 
-  Serial.println(timeServerIP);
-  Serial.println("\r\nSending NTP request ...");
-  sendNTPpacket(timeServerIP);  
-
-
   // define PIN Modes
   pinMode(D0, OUTPUT);
   
   // register handlers
-  server.on("/toggle", toggle);
+  server.on("/toggle1", toggle_1);
+  server.on("/on1", on_1);
+  server.on("/off1", off_1);
+  server.on("/toggle0", toggle_0);
+  server.on("/on0", on_0);
+  server.on("/off0", off_0);
   server.on("/timers", timers);
   server.on("/sensors", readSensors);
  
@@ -254,15 +338,22 @@ void setup() {
   server.begin();
   Serial.println("Server started");
 
-    // Print the IP address
+  // Print the IP address
   Serial.print("Use this URL to connect: ");
   Serial.print("http://");
   Serial.print(WiFi.localIP());
+  Serial.println(timeServerIP);
+  Serial.println("\r\nSending NTP request ...");
+  sendNTPpacket(timeServerIP);  
 }
 
-
+uint32_t lastTriggerTime=0;
 void loop() {
-  uint32_t actual_time = updateTimes();
-  triggerCallbacks(actual_time);
+  uint32_t actual_time = updateTimes()/10; //TODO: bad !!
+  // call trigger at max once a second
+  if(lastTriggerTime != actual_time){
+    lastTriggerTime = actual_time;
+    triggerCallbacks(actual_time*10);
+  }
   server.handleClient();
 }
